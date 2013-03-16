83d3a43b08496a4308aa8550b688934ed391ce42
Jiri Pirko <jiri@resnulli.us>
2013-03-16 17:37:07 +0100

    lib: introduce ndp_msg_type_info() helper
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- libndp/libndp.c
+++ libndp/libndp.c
@@ -705,13 +705,18 @@ static struct ndp_msg_type_info ndp_msg_type_info_list[] =
 
 #define NDP_MSG_TYPE_LIST_SIZE ARRAY_SIZE(ndp_msg_type_info_list)
 
+struct ndp_msg_type_info *ndp_msg_type_info(enum ndp_msg_type msg_type)
+{
+	return &ndp_msg_type_info_list[msg_type];
+}
+
 static int ndp_msg_type_by_raw_type(enum ndp_msg_type *p_msg_type,
 				    uint8_t raw_type)
 {
 	int i;
 
 	for (i = 0; i < NDP_MSG_TYPE_LIST_SIZE; i++) {
-		if (ndp_msg_type_info_list[i].raw_type == raw_type) {
+		if (ndp_msg_type_info(i)->raw_type == raw_type) {
 			*p_msg_type = i;
 			return 0;
 		}
@@ -733,10 +738,9 @@ static struct ndp_msg *ndp_msg_alloc(void)
 
 static void ndp_msg_init(struct ndp_msg *msg, enum ndp_msg_type msg_type)
 {
-	struct ndp_msg_type_info *type_info = &ndp_msg_type_info_list[msg_type];
-
 	msg->type = msg_type;
-	msg->opts_start = msg->buf + type_info->raw_struct_size;
+	msg->opts_start = msg->buf +
+			  ndp_msg_type_info(msg_type)->raw_struct_size;
 }
 
 /**
