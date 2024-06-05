jpirko/libndp

commit 8296a5bf075543eef2167afdfecb222e70866744

add support for Route Information Option (rfc4191)
Signed-off-by: Jiri Pirko <jiri@resnulli.us>

libndp/libndp.c
@@ -960,6 +960,9 @@ static struct ndp_msg_opt_type_info ndp_msg_opt_type_info_list[] =
 	[NDP_MSG_OPT_MTU] = {
 		.raw_type = ND_OPT_MTU,
 	},
+	[NDP_MSG_OPT_ROUTE] = {
+		.raw_type = __ND_OPT_ROUTE_INFO,
+	},
 };
 
 #define NDP_MSG_OPT_TYPE_LIST_SIZE ARRAY_SIZE(ndp_msg_opt_type_info_list)
@@ -1141,7 +1144,7 @@ uint32_t ndp_msg_opt_prefix_valid_time(struct ndp_msg *msg, int offset)
 	struct nd_opt_prefix_info *pi =
 			ndp_msg_payload_opts_offset(msg, offset);
 
-	return  ntohl(pi->nd_opt_pi_valid_time);
+	return ntohl(pi->nd_opt_pi_valid_time);
 }
 
 /**
@@ -1177,6 +1180,86 @@ uint32_t ndp_msg_opt_mtu(struct ndp_msg *msg, int offset)
 	struct nd_opt_mtu *mtu = ndp_msg_payload_opts_offset(msg, offset);
 
 	return ntohl(mtu->nd_opt_mtu_mtu);
+}
+
+/**
+ * ndp_msg_opt_route_prefix:
+ * @msg: message structure
+ *
+ * Get route prefix addr.
+ * User should use this function only inside ndp_msg_opt_for_each_offset()
+ * macro loop.
+ *
+ * Returns: address.
+ **/
+NDP_EXPORT
+struct in6_addr *ndp_msg_opt_route_prefix(struct ndp_msg *msg, int offset)
+{
+	static struct in6_addr prefix;
+	struct __nd_opt_route_info *ri =
+			ndp_msg_payload_opts_offset(msg, offset);
+
+	memset(&prefix, 0, sizeof(prefix));
+	memcpy(&prefix, &ri->nd_opt_ri_prefix, (ri->nd_opt_ri_len - 1) << 3);
+	return &prefix;
+}
+
+/**
+ * ndp_msg_opt_route_prefix_len:
+ * @msg: message structure
+ *
+ * Get route prefix length.
+ * User should use this function only inside ndp_msg_opt_for_each_offset()
+ * macro loop.
+ *
+ * Returns: length of route prefix.
+ **/
+NDP_EXPORT
+uint8_t ndp_msg_opt_route_prefix_len(struct ndp_msg *msg, int offset)
+{
+	struct __nd_opt_route_info *ri =
+			ndp_msg_payload_opts_offset(msg, offset);
+
+	return ri->nd_opt_ri_prefix_len;
+}
+
+/**
+ * ndp_msg_opt_route_lifetime:
+ * @msg: message structure
+ *
+ * Get route lifetime.
+ * User should use this function only inside ndp_msg_opt_for_each_offset()
+ * macro loop.
+ *
+ * Returns: route lifetime in seconds, (uint32_t) -1 means infinity.
+ **/
+NDP_EXPORT
+uint32_t ndp_msg_opt_route_lifetime(struct ndp_msg *msg, int offset)
+{
+	struct __nd_opt_route_info *ri =
+			ndp_msg_payload_opts_offset(msg, offset);
+
+	return ntohl(ri->nd_opt_ri_lifetime);
+}
+
+/**
+ * ndp_msg_opt_route_preference:
+ * @msg: message structure
+ *
+ * Get route preference.
+ * User should use this function only inside ndp_msg_opt_for_each_offset()
+ * macro loop.
+ *
+ * Returns: route preference.
+ **/
+NDP_EXPORT
+enum ndp_route_preference
+ndp_msg_opt_route_preference(struct ndp_msg *msg, int offset)
+{
+	struct __nd_opt_route_info *ri =
+			ndp_msg_payload_opts_offset(msg, offset);
+
+	return (ri->nd_opt_ri_prf_reserved >> 3) & 3;
 }
 
 static int ndp_call_handlers(struct ndp *ndp, struct ndp_msg *msg);

include/ndp.h
@@ -52,6 +52,12 @@ enum ndp_msg_type {
 	NDP_MSG_ALL, /* Matches all */
 };
 
+enum ndp_route_preference {
+	NDP_ROUTE_PREF_LOW = 3,
+	NDP_ROUTE_PREF_MEDIUM = 0,
+	NDP_ROUTE_PREF_HIGH = 1,
+};
+
 int ndp_msg_new(struct ndp_msg **p_msg, enum ndp_msg_type msg_type);
 void ndp_msg_destroy(struct ndp_msg *msg);
 void *ndp_msg_payload(struct ndp_msg *msg);
@@ -96,6 +102,7 @@ enum ndp_msg_opt_type {
 	NDP_MSG_OPT_PREFIX, /* Prefix Information */
 	NDP_MSG_OPT_REDIR, /* Redirected Header */
 	NDP_MSG_OPT_MTU, /* MTU */
+	NDP_MSG_OPT_ROUTE, /* Route Information */
 };
 
 int ndp_msg_next_opt_offset(struct ndp_msg *msg, int offset,
@@ -117,6 +124,12 @@ uint32_t ndp_msg_opt_prefix_valid_time(struct ndp_msg *msg, int offset);
 uint32_t ndp_msg_opt_prefix_preferred_time(struct ndp_msg *msg, int offset);
 
 uint32_t ndp_msg_opt_mtu(struct ndp_msg *msg, int offset);
+
+struct in6_addr *ndp_msg_opt_route_prefix(struct ndp_msg *msg, int offset);
+uint8_t ndp_msg_opt_route_prefix_len(struct ndp_msg *msg, int offset);
+uint32_t ndp_msg_opt_route_lifetime(struct ndp_msg *msg, int offset);
+enum ndp_route_preference
+ndp_msg_opt_route_preference(struct ndp_msg *msg, int offset);
 
 typedef int (*ndp_msgrcv_handler_func_t)(struct ndp *ndp, struct ndp_msg *msg,
 					 void *priv);

libndp/ndp_private.h
@@ -78,8 +78,19 @@ ndp_log_null(struct ndp *ndp, const char *format, ...) {}
 #endif
 
 /**
- * SECTION: function prototypes
- * @short_description: prototypes for internal functions
+ * SECTION: netinet/icmp6.h addendum
+ * @short_description: defines and structs missing from netinet/icmp6.h
  */
+
+#define __ND_OPT_ROUTE_INFO 24 /* rfc4191 */
+
+struct __nd_opt_route_info { /* route information */
+	uint8_t		nd_opt_ri_type;
+	uint8_t		nd_opt_ri_len;
+	uint8_t		nd_opt_ri_prefix_len;
+	uint8_t		nd_opt_ri_prf_reserved;
+	uint32_t	nd_opt_ri_lifetime;
+	char		nd_opt_ri_prefix[0];
+};
 
 #endif /* _NDP_PRIVATE_H_ */

utils/ndptool.c
@@ -235,6 +235,32 @@ static int msgrcv_handler_func(struct ndp *ndp, struct ndp_msg *msg, void *priv)
 		}
 		ndp_msg_opt_for_each_offset(offset, msg, NDP_MSG_OPT_MTU)
 			pr_out("  MTU: %u\n", ndp_msg_opt_mtu(msg, offset));
+		ndp_msg_opt_for_each_offset(offset, msg, NDP_MSG_OPT_ROUTE) {
+			uint32_t lifetime;
+
+			lifetime = ndp_msg_opt_route_lifetime(msg, offset);
+			pr_out("  Route: %s/%u",
+			       str_in6_addr(ndp_msg_opt_route_prefix(msg, offset)),
+			       ndp_msg_opt_route_prefix_len(msg, offset));
+			pr_out(", lifetime: ");
+			if (lifetime == (uint32_t) -1)
+				pr_out("infinity");
+			else
+				pr_out("%us", lifetime);
+			pr_out(", preference: ");
+			switch (ndp_msg_opt_route_preference(msg, offset)) {
+			case NDP_ROUTE_PREF_LOW:
+				pr_out("low");
+				break;
+			case NDP_ROUTE_PREF_MEDIUM:
+				pr_out("medium");
+				break;
+			case NDP_ROUTE_PREF_HIGH:
+				pr_out("high");
+				break;
+			}
+			pr_out("\n");
+		}
 	} else if (msg_type == NDP_MSG_NS) {
 		pr_out("  Type: NS\n");
 	} else if (msg_type == NDP_MSG_NA) {

commit 05e4ba7b0d126eea4c04387dcf40596059ee24af

libndp: valid route information option length
RFC 4191 specifies that the Route Information Option Length should be 1, 2,
or 3, depending on the Prefix Length. A malicious node could potentially
trigger a buffer overflow and crash the tool by sending an IPv6 router
advertisement message containing the "Route Information" option with a
"Length" field larger than 3.

To address this, add a check on the length field.

Fixes: 8296a5bf0755 ("add support for Route Information Option (rfc4191)")
Reported-by: Evgeny Vereshchagin <evverx@gmail.com>
Suggested-by: Felix Maurer <fmaurer@redhat.com>
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
Signed-off-by: Jiri Pirko <jiri@nvidia.com>

libndp/libndp.c
@@ -1231,6 +1231,17 @@ static bool ndp_msg_opt_route_check_valid(void *opt_data)
 	 */
 	if (((ri->nd_opt_ri_prf_reserved >> 3) & 3) == 2)
 		return false;
+
+	/* The Length field is 1, 2, or 3 depending on the Prefix Length.
+	 * If Prefix Length is greater than 64, then Length must be 3.
+	 * If Prefix Length is greater than 0, then Length must be 2 or 3.
+	 * If Prefix Length is zero, then Length must be 1, 2, or 3.
+	 */
+	if (ri->nd_opt_ri_len > 3 ||
+	    (ri->nd_opt_ri_prefix_len > 64 && ri->nd_opt_ri_len != 3) ||
+	    (ri->nd_opt_ri_prefix_len > 0 && ri->nd_opt_ri_len == 1))
+		return false;
+
 	return true;
 }
 